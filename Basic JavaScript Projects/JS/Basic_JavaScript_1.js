4eb3324909b73df397808169c50a435f67de6e2d
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-06-14 16:44:51 -0700

    Add JavaScript project
    For instructor review

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+var A = "This is a";            //This is my first string variable 
+var B = " test";                //This is my second string variable 
+var B = B.fontcolor("red");     //This is changing the font color of "B" to red
+document.write(A+B);            //This is writing the expression to the page
+alert("This page will say it is a test, don\'t beleive it.")  //This is the alert box and its text
