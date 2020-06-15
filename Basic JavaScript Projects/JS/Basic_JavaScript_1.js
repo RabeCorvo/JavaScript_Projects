12c7a34360152399382ce72d1d57049ba47f4719
Samuel Breidenbach <sambreidenbach@gmail.com>
2020-06-15 12:19:15 -0700

    Add Basic_JavaScript_2 Assignment
    JavaScript 2 assignment

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -1,5 +1,7 @@
-var A = "This is a";            //This is my first string variable 
-var B = " test";                //This is my second string variable 
-var B = B.fontcolor("red");     //This is changing the font color of "B" to red
-document.write(A+B);            //This is writing the expression to the page
-alert("This page will say it is a test, don\'t beleive it.")  //This is the alert box and its text
+
+var x = myFunction(4,3);
+document.getElementById("demo").innerHTML = x;
+
+function myFunction(a, b) {
+    return a + b;
+}
---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function Function_1() {
+    var dog = "My dog is named ";
+    dog += "Gus."
+    document.getElementById("pet").innerHTML = dog
+}
